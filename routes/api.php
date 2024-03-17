50cd06b1843f2e4f228c3506acc6a985476526c2
Mehedi Hasan <66936454+mh-majumdar@users.noreply.github.com>
2024-03-18 04:14:34 +0600

    api-learning

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+use App\Http\Controllers\Api\ApiController;
+
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function () {
+
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("refresh", [ApiController::class, "refreshToken"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
